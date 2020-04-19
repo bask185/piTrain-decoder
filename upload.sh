38233bb0cc75ff72bc7f7ea48695f49a85a2091e
bask185 <sebastiaan_knippels4@hotmail.com>
2020-04-19 22:25:34 +0200

    comit voor push

---
+++ upload.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+arduino-cli compile -p "COM4" -b arduino:avr:nano ~/Documents/software/piTrain-decoder
+arduino-cli upload -b arduino:avr:nano:cpu=atmega328old -p COM3 -i ~/Documents/software/piTrain-decoder/piTrain-decoder.arduino.avr.nano.hex
+rm *.hex *.elf
+exit
